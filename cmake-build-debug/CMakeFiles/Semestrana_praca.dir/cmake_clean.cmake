c1bf4fc4fa60599d20df64b6c6df32d4abc7c874
Citronik <58891611+Citronik@users.noreply.github.com>
2022-01-04 10:20:29 +0100

    aaamen

---
+++ cmake-build-debug/CMakeFiles/Semestrana_praca.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Semestrana_praca.dir/main.c.o"
+  "Semestrana_praca"
+  "Semestrana_praca.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Semestrana_praca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Semestrana_praca.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Semestrana_praca.
+# This may be replaced when dependencies are built.
